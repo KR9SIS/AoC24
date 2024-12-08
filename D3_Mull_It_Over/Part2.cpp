4bf510ad5f54b503d0fb5aafb2eca378f69e9f81
kr9sis <kristoferhelgi@protonmail.com>
2024-12-08 13:04:32 +0000

    Finished day 3 part 2

--- D3_Mull_It_Over/Part2.cpp
+++ D3_Mull_It_Over/Part2.cpp
@@ -5,17 +5,18 @@
 
 int main(int argc, char *argv[]) {
   std::fstream my_file;
-  std::string filename = "small_input.txt";
+  std::string filename = "d3_input.txt";
   my_file.open(filename, std::ios::in);
   if (!my_file.is_open()) {
     std::cout << "File not found: " << filename << std::endl;
     return 1;
   }
 
-  std::regex rgx("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
+  std::regex rgx("(mul\\([0-9]{1,3},[0-9]{1,3}\\))|(don't\\(\\))|(do\\(\\))");
   std::string file_line;
   std::vector<std::string> matches;
   int sum = 0;
+  bool mul = true;
   while (std::getline(my_file, file_line)) {
     auto matches_start =
         std::sregex_iterator(file_line.begin(), file_line.end(), rgx);
@@ -24,17 +25,26 @@ int main(int argc, char *argv[]) {
     for (std::sregex_iterator k = matches_start; k != matches_end; ++k) {
       std::smatch match = *k;
       std::string match_str = match.str();
-      int open = match_str.find("(");
-      int mid = match_str.find(",");
-      int close = match_str.find(")");
-      // std::cout << match_str << "\nopen: " << open << "\nmid: " << mid
-      //           << "\nclose: " << close << "\n";
+      if (match_str == "do()") {
+        mul = true;
+        continue;
+      } else if (match_str == "don't()") {
+        mul = false;
+      }
 
-      int left = std::stoi(match_str.substr(open + 1, open + 1 - mid));
-      int right = std::stoi(match_str.substr(mid + 1, mid + 1 - close));
-      sum = sum + (left * right);
-      // std::cout << "\nleft: " << left << "\nright: " << right << "\n"
-      //           << std::endl;
+      if (mul) {
+        int open = match_str.find("(");
+        int mid = match_str.find(",");
+        int close = match_str.find(")");
+        // std::cout << match_str << "\nopen: " << open << "\nmid: " << mid
+        //           << "\nclose: " << close << "\n";
+
+        int left = std::stoi(match_str.substr(open + 1, open + 1 - mid));
+        int right = std::stoi(match_str.substr(mid + 1, mid + 1 - close));
+        sum = sum + (left * right);
+        // std::cout << "\nleft: " << left << "\nright: " << right << "\n"
+        //           << std::endl;
+      }
     }
   }
   std::cout << "Total sum is: " << sum << std::endl;
