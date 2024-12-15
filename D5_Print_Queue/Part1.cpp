26562d63b13b1f5854a9f0e9bd5b3438720117ca
kr9sis <kristoferhelgi@protonmail.com>
2024-12-15 09:57:15 +0000

    Finished file reading logic

--- D5_Print_Queue/Part1.cpp
+++ D5_Print_Queue/Part1.cpp
@@ -11,6 +11,24 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  PriorityQueue p_queue;
+  std::string file_line;
+  int p_page, c_page;
+  PageNode *p, *c;
+  while (std::getline(my_file, file_line)) {
+    if (file_line.empty()) {
+      break;
+    }
+    int delim = file_line.find("|");
+    p_page = std::stoi(file_line.substr(0, delim));
+    c_page = std::stoi(file_line.erase(0, delim + 1));
+    p = p_queue.find(p_page);
+    c = p_queue.find(c_page);
+  }
+  while (std::getline(my_file, file_line)) {
+    std::cout << file_line << "\n";
+  }
+  std::cout << std::endl;
   my_file.close();
   return 0;
 }
