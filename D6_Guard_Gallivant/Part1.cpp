b3f0766b7cd74ff2473b93a5c961d4c833cf36df
kr9sis <kristoferhelgi@protonmail.com>
2024-12-24 16:16:56 +0000

    Started working on day 6, set up way to create, maintain and print out grid

--- D6_Guard_Gallivant/Part1.cpp
+++ D6_Guard_Gallivant/Part1.cpp
@@ -1,7 +1,46 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <utility>
+#include <vector>
 
+class Grid {
+  std::vector<std::vector<char>> grid;
+  int width = 0;
+  std::pair<int, int> guard_coords;
+
+public:
+  void insert_row(std::string new_row) {
+    if (this->width == 0) {
+      this->width = new_row.size();
+    } else if (new_row.size() != this->width) {
+      std::cout << new_row << "\nNew row length of " << new_row.size()
+                << "\ndoes not fit in grid with row length of " << this->width
+                << std::endl;
+    }
+    std::vector<char> row;
+    for (int i = 0; i < new_row.size(); i++) {
+      char c = new_row[i];
+      if (c == '^') {
+        this->guard_coords.first = i;
+        this->guard_coords.second = this->grid.size();
+      }
+      row.push_back(c);
+    }
+    this->grid.push_back(row);
+  }
+
+  void print_grid() {
+    for (int row = 0; row < this->grid.size(); row++) {
+      for (int col = 0; col < this->width; col++) {
+        std::cout << this->grid[row][col] << " ";
+      }
+      std::cout << "\n";
+    }
+    std::cout << "Guard is at " << this->guard_coords.first << ", "
+              << this->guard_coords.second << std::endl;
+  }
+};
 int main(int argc, char *argv[]) {
   std::fstream my_file;
   std::string filename = "small_input.txt";
@@ -11,6 +50,13 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  Grid grid = Grid();
+  std::string line;
+  while (std::getline(my_file, line)) {
+    grid.insert_row(line);
+  }
   my_file.close();
+
+  grid.print_grid();
   return 0;
 }
